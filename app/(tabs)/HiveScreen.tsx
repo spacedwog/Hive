81aad1f8ddac054958b06223bc725cf5a1cb0871
Spacedwog <owner@spacedwog.com.br>
2025-08-08 07:05:27 -0300

    Commited

--- app/(tabs)/HiveScreen.tsx
+++ app/(tabs)/HiveScreen.tsx
@@ -10,7 +10,7 @@ import {
 } from 'react-native';
 
 const nodes = [
-  { name: 'ESP32', ip: '192.168.15.166' },
+  { name: 'ESP32_VESPA', ip: '192.168.15.166' }, // ✅ Substitua com IP real do ESP32
 ];
 
 type NodeStatus = {
