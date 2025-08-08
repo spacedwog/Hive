6acdf2b91712b999a8df8852e51f4a7ac63246d2
Spacedwog <owner@spacedwog.com.br>
2025-08-08 11:13:59 -0300

    Commited

--- src/victory-native.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'victory-native' {
-  import * as React from 'react';
-
-  export const VictoryChart: React.ComponentType<any>;
-  export const VictoryLine: React.ComponentType<any>;
-  export const VictoryAxis: React.ComponentType<any>;
-  export const VictoryTheme: any;
-}
