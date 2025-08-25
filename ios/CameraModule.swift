65d233fc6088accc0860014791207634a9458fc2
Spacedwog <owner@spacedwog.com.br>
2025-08-25 13:04:39 -0300

    Commited

--- ios/CameraModule.swift
+++ ios/CameraModule.swift
@@ -1,15 +1,18 @@
 import Foundation
 import UIKit
-import React
 
 @objc(CameraModule)
 class CameraModule: NSObject {
-  
+  @objc
+  static func requiresMainQueueSetup() -> Bool {
+    return true
+  }
+
   @objc(openCamera:rejecter:)
   func openCamera(_ resolve: @escaping RCTPromiseResolveBlock,
                   rejecter reject: @escaping RCTPromiseRejectBlock) {
     DispatchQueue.main.async {
-      guard let rootVC = UIApplication.shared.keyWindow?.rootViewController else {
+      guard let rootVC = UIApplication.shared.delegate?.window??.rootViewController else {
         reject("E_NO_VIEW", "Não foi possível acessar a rootViewController", nil)
         return
       }
@@ -18,10 +21,9 @@ class CameraModule: NSObject {
         let picker = UIImagePickerController()
         picker.sourceType = .camera
         picker.allowsEditing = false
-        picker.delegate = rootVC as? UIImagePickerControllerDelegate & UINavigationControllerDelegate
-        rootVC.present(picker, animated: true, completion: {
+        rootVC.present(picker, animated: true) {
           resolve("Câmera aberta com sucesso")
-        })
+        }
       } else {
         reject("E_NO_CAMERA", "Câmera não disponível neste dispositivo", nil)
       }
