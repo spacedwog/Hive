35f020b60297b7c826ed86ad14182b28312e2b09
Spacedwog <owner@spacedwog.com.br>
2025-08-25 13:10:31 -0300

    Commited

--- ios/CameraModule.m
+++
@@ -1,6 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface RCT_EXTERN_MODULE(CameraModule, NSObject)
-RCT_EXTERN_METHOD(openCamera:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
-@end
