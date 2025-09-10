273e64605bda848783cac1b24f853d8635298d48
Spacedwog <owner@spacedwog.com.br>
2025-09-10 16:40:33 -0300

    Commited

--- api/status.ts
+++
@@ -1,9 +0,0 @@
-// api/status.ts
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({
-    message: "Olá do Vercel!",
-    timestamp: Date.now(),
-  });
-}
