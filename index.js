de150e155ff7cda57785f4137b70c7364caa7b13
Spacedwog <owner@spacedwog.com.br>
2025-09-17 16:27:01 -0300

    Commited

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import IndexApiHandler from "./indexApiHandler.js";
+import IndexApiHandler from "./lib/handler/indexApiHandler.js";
 
 export default function handler(req, res) {
   IndexApiHandler.handle(req, res);
