04d03f1b2b027294e7e825e25add0d383ee1f67b
Spacedwog <owner@spacedwog.com.br>
2025-09-17 15:54:09 -0300

    Commited

--- api/sensor.js
+++ api/sensor.js
@@ -88,8 +88,9 @@ export default async function handler(req, res) {
         return res.status(200).json(successResponse("Dados do sensor", data));
       }
 
-      if (info === "server") 
+      if (info === "server") {
         return res.status(200).json(successResponse("Dados do servidor", getServerInfo()));
+      }
 
       return res.status(400).json(errorResponse("INVALID_PARAM", `Valor inválido para 'info': ${info}`));
     }
