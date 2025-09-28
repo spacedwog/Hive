bf9d1526a729a8fa0e396e07bddbd7117e35ae9a
Spacedwog <owner@spacedwog.com.br>
2025-09-28 12:12:03 -0300

    Commited

--- babel.config.js
+++ babel.config.js
@@ -8,9 +8,6 @@ module.exports = function (api) {
         {
           moduleName: '@env',
           path: '.env',
-          blocklist: null,
-          allowlist: null,
-          safe: false,
           allowUndefined: true,
         },
       ],
--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,11 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
+// metro.config.js
+// Learn more: https://docs.expo.dev/guides/customizing-metro/
 const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+// 🔑 Reanimated precisa que o Metro reconheça arquivos .cjs
+config.resolver.sourceExts.push('cjs');
 
 module.exports = config;
