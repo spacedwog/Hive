24b763fa56bc41f294472991a1f9ec27a45f6507
Spacedwog <owner@spacedwog.com.br>
2025-09-28 12:14:37 -0300

    Commited

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,5 @@
-// metro.config.js
-// Learn more: https://docs.expo.dev/guides/customizing-metro/
 const { getDefaultConfig } = require('expo/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
-
-// 🔑 Reanimated precisa que o Metro reconheça arquivos .cjs
-config.resolver.sourceExts.push('cjs');
-
+config.resolver.sourceExts.push('cjs'); // necessário pro Reanimated
 module.exports = config;
