b23d5e293219c14e003c91a9d108a943a914b4ae
Spacedwog <owner@spacedwog.com.br>
2025-09-30 14:38:25 -0300

    Commited

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 export default function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+    return res.status(200).json({ received: data });
+  }
+
   res.status(200).json({ message: "Hello from HIVE!" });
 }
