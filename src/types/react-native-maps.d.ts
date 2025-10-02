1696ba4a1ff5cbba47e16c5ac57999969b941681
Spacedwog <owner@spacedwog.com.br>
2025-10-01 21:11:14 -0300

    Commited

---
+++ src/types/react-native-maps.d.ts
@@ -0,0 +1,13 @@
+// types/react-native-maps.d.ts
+declare module 'react-native-maps' {
+  import { ComponentType } from 'react';
+  export const MapView: ComponentType<any>;
+  export default MapView;
+}
+
+// types/react-native-community-slider.d.ts
+declare module '@react-native-community/slider' {
+  import { ComponentType } from 'react';
+  const Slider: ComponentType<any>;
+  export default Slider;
+}
