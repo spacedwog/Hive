dff88ed05d1e3e3be3569fc667e96b5208b7d808
Spacedwog <owner@spacedwog.com.br>
2025-10-02 12:17:05 -0300

    Commited

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,6 @@
 declare module '@env' {
   export const AUTH_USERNAME: string;
+  export const MY_PAT: string;
   export const AUTH_PASSWORD: string;
   export const GITHUB_TOKEN: string;
   export const GITHUB_OWNER: string;
