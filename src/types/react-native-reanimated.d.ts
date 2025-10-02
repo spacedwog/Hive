7dcdce94202c066f9a4e7846b769def9b4e7e982
Spacedwog <owner@spacedwog.com.br>
2025-10-01 21:20:12 -0300

    Commited

--- src/types/react-native-reanimated.d.ts
+++ src/types/react-native-reanimated.d.ts
@@ -3,5 +3,15 @@ declare module 'react-native-reanimated' {
   const Animated: ComponentType<any>;
   export const View: ComponentType<any>;
   export const ScrollView: ComponentType<any>;
+
+  export function useAnimatedStyle(cb: () => any, deps?: any[]): any;
+  export function useSharedValue<T = any>(value: T): { value: T };
+  export function withTiming(value: any, config?: any, callback?: () => void): any;
+  export function withRepeat(animation: any, times?: number, reverse?: boolean): any;
+  export function withSequence(...animations: any[]): any;
+  export function interpolate(value: number, inputRange: number[], outputRange: number[]): number;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  export function useAnimatedRef<T>(): any;
+
   export default Animated;
 }
