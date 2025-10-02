d911a7e705517f29d2d6bedc1eee8792cc870105
Spacedwog <owner@spacedwog.com.br>
2025-10-01 22:55:31 -0300

    Commited

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,7 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
-import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
-import { useColorScheme } from '../hooks/useColorScheme.ts';
-
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -15,12 +11,17 @@ export default function RootLayout() {
   if (!loaded) return null;
 
   return (
-    <NavigationContainer theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    <>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          // Aplica tema se quiser, mas normalmente o Expo Router gerencia automaticamente
+        }}
+      >
+        <Stack.Screen name="(tabs)" />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
-    </NavigationContainer>
+    </>
   );
 }
